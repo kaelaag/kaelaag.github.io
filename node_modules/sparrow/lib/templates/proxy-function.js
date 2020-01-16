311f9e0b6979ab92d1e2797b24cec88046ee0ea4
Kaela Gobencion <kaelaag@bu.edu>
2020-01-16 00:59:54 -0500

    Updated UI/UX

---
+++ node_modules/sparrow/lib/templates/proxy-function.js
@@ -0,0 +1,32 @@
+﻿(
+    function({{parameters}})
+    {
+        var routeValues = 
+        {
+            {{#each routeParameterNames}}
+                '{{this}}': {{this}},
+            {{/each}}
+        };
+
+        var url = '/service/{{route}}';
+
+        for(var routeValueKey in routeValues)
+        {
+            url = url.replaceAll('{' + routeValueKey + '}', String(routeValues[routeValueKey]));
+        }
+
+        $.ajax(
+        {
+            url: url,
+
+            type: '{{method}}',
+
+            data: typeof body !== 'undefined' ? body : undefined,
+
+            success: function (result)
+            {
+                respond(result);
+            }
+        });
+    }
+)
---
+++ node_modules/sparrow/lib/templating.js
@@ -0,0 +1,34 @@
+﻿
+require('./extensions');
+
+var Handlebars = require('handlebars');
+
+var internal =
+{
+    loadTemplates: function ()
+    {
+        var fs = require('fs');
+
+        var templatesFolder = __dirname + '\\templates';
+
+        var templateFilenames = fs.readdirSync(templatesFolder);
+
+        var templates = {};
+
+        for (var i = 0; i < templateFilenames.length; i++)
+        {
+            var templateFilename = templateFilenames[i];
+
+            var contents = fs.readFileSync(templatesFolder + '\\' + templateFilename, 'utf8');
+
+            templates[templateFilename] = Handlebars.compile(contents);
+        }
+
+        return templates;
+    }
+};
+
+module.exports =
+{
+    templates: internal.loadTemplates()
+};
